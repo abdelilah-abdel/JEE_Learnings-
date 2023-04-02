5be30f53949c34ce03d15327b6b4b8e468b94d02
abdelilah-abdel <abdelilahezabdel@gmail.com>
2023-04-02 05:06:57 +0000

    spring

---
+++ Jee_spring_test/src/main/java/com/example/jee_spring_test/JeeSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.jee_spring_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeeSpringTestApplication.class, args);
+    }
+
+}
---
+++ Jee_spring_test/src/test/java/com/example/jee_spring_test/JeeSpringTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jee_spring_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeSpringTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
